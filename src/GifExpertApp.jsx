d891c4b0a276dfa640a59052cab307074102e05c
JordyV23 <jordyvargas0123@gmail.com>
2023-03-31 16:38:05 -0600

    implementacion de useState

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,8 +1,17 @@
-
+import { useState } from "react";
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(['One Punch', 'Nanatsu no Taizai']);
+
   return (
-   <>
-    <h1>GifExpertApp</h1>
-   </>
-  )
-}
+    <>
+      <h1>GifExpertApp</h1>
+
+      <ol>
+        {categories.map( category => {
+          return <li key={category}>{category}</li>
+        })
+        }
+      </ol>
+    </>
+  );
+};
