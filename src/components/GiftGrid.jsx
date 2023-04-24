fc2d36d6ed38ce2313ccc6708e6e932d29ceff61
JordyV23 <jordyvargas0123@gmail.com>
2023-04-24 11:49:25 -0600

    Componente de 'GifItem' creado

--- src/components/GiftGrid.jsx
+++ src/components/GiftGrid.jsx
@@ -1,32 +1,30 @@
 import { useEffect } from "react";
-import { getGifs } from "../helpers/getGifs";
 import { useState } from "react";
+import { getGifs } from "../helpers/getGifs";
+import { GifItem } from "./GifItem";
 
 export const GiftGrid = ({ category }) => {
   const [images, setImages] = useState([]);
 
-  const getImages  = async() => {
-    const newImages = await getGifs(category)
-    setImages(newImages)
-  }
-
+  const getImages = async () => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+  };
 
   useEffect(() => {
-    getImages()
+    getImages();
   }, []);
 
   return (
     <>
       <h3>{category}</h3>
 
-      <ol>
-        {
-            images.map(image=>(
-                <li key={image.id}>{image.title}</li>
-            ))
-        }
-        
-      </ol>
+      <div className="card-grid">
+        {images.map((image) => (
+          <GifItem key={image.id} { ...image } 
+          />
+        ))}
+      </div>
     </>
   );
 };
